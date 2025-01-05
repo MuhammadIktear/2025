c0be8d3ba308be8513010bd7353a87593de1ea3f
Muhammad Iktear <iktear500@gmail.com>
2025-01-06 02:17:55 +0600

    6

---
+++ January/merge_sort.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[],int l,int m,int r){
+    int lsz=(m-l)+1;
+    int rsz=r-m;
+    int L[lsz];
+    int R[rsz];
+    int k=0;
+    for(int i=l;i<=m;i++){
+        L[k]=arr[i];
+        k++;
+    }
+    k=0;
+    for(int i=m+1;i<=r;i++){
+        R[k]=arr[i];
+        k++;
+    }
+    int i=0,j=0;
+    int cur=l;
+    while(i<lsz&&j<rsz){
+        if(L[i]<=R[j]){
+            arr[cur]=L[i];
+            i++;
+        }
+        else{
+            arr[cur]=R[j];
+            j++;        
+        }
+        cur++;
+    }
+    while(i<lsz){
+        arr[cur]=L[i];
+        i++;
+        cur++;
+    } 
+    while(j<rsz){
+        arr[cur]=R[j];
+        j++;
+        cur++;
+    }       
+}
+
+void merge_sort(int arr[],int l,int r){
+    if(l<r){
+        int mid=(l+r)/2;
+        merge_sort(arr,l,mid);
+        merge_sort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    merge_sort(arr,0,n-1);
+    for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+}
